2ebd2bdb9e1b155f082c485eb2f2e6b83155864e
LeeSangHoon <dl57934@naver.com>
2018-06-04 14:48:25 +0900

    complete make data set peach, apple, banana

--- GetBananaData.py
+++ GetBananaData.py
@@ -1,15 +1,25 @@
 from urllib import request
-import requests
+from selenium import webdriver
 from bs4 import BeautifulSoup as bs
+import time
 
-session = requests.session()
+url= 'https://search.daum.net/search?nil_suggest=btn&nil_ch=&rtupcoll=&w=img&m=&f=&lpp=&DA=SBC&sug=&sq=&o=&sugo=&q=%EA%B3%BC%EC%9D%BC+%EB%B0%B0'
+browser =  webdriver.Chrome(executable_path='./chromedriver.exe')
+browser.implicitly_wait(3)
+browser.get(url)
+lastLength = browser.execute_script('return document.body.scrollHeight;')
+for i in range(30):
+    browser.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+    time.sleep(0.5)
 
-html = session.get('https://search.naver.com/search.naver?where=image&sm=tab_jum&query=%EB%B0%94%EB%82%98%EB%82%98')
-print(html.text)
-getHtml = bs(html.text, 'html.parser')
+getHtml = bs(browser.page_source, 'html.parser')
 bananaSrc = getHtml.findAll('img')
-bananaImgTag = bananaSrc[1:]
-print(bananaImgTag)
-for i, banana in enumerate(bananaImgTag):
-     request.urlretrieve(banana.get('src'), './bananaData/'+str(i)+'.jpg')
+bananaSrc = bananaSrc[2:]
 
+
+for i, banana in enumerate(bananaSrc):
+    try:
+        print(banana.get('src'))
+        request.urlretrieve(banana.get('src'), './peach/'+str(i)+'.jpg')
+    except:
+        pass
